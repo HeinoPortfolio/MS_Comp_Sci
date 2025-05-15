79724af0be7888d9d373f3978e897bbb23abdef6
Matthew Heino <61652462+HeinoPortfolio@users.noreply.github.com>
2025-05-15 05:43:52 -0400

    Add files via upload

---
+++ 6 Data Mining and Machine Learning/Assignments/Module_11/Module 11 Question 2.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 15 04:08:30 2025
+
+@author: ntcrw
+"""
+
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
+
+#------------------------------------------------------------------------------
+# Pre-question work -----------------------------------------------------------
+
+transactions = [['A', 'B', 'C'], ['A', 'C'], ['A', 'D'], ['B','E','F']]
+
+
+# Convert to string
+transactions = [[str(item) for item in transaction] for transaction in transactions]
+
+# Create the transaction encoder ----------------------------------------------
+tran_enc = TransactionEncoder()
+
+# Create transaction array with each elment -----------------------------------
+te_array = tran_enc.fit(transactions).transform(transactions)
+    
+# Create a dataframe to show the results of the encoding / transformation -----
+
+    
+encoded_transactions_df = pd.DataFrame(te_array, columns=tran_enc.columns_)
+
+#print(encoded_transactions_df)
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#
+# Question 2:
+#
+# With min_support = 0.5, what is the confidence of rule C->A? 
+# 
+
+# Find frequent itemsets with minimum support of 0.2 -------------------------
+frequent_itemsets = apriori(encoded_transactions_df, min_support=0.5, 
+                            use_colnames=True)
+    
+
+#print("Frequent item sets: ", frequent_itemsets)
+
+
+# Generate association rules with minimum confidence of 0.5 -------------------
+   
+assoc_rules = association_rules(frequent_itemsets, metric="confidence", 
+                          min_threshold=0.5)
+
+#print("\n Question1: \n", rules)
+
---
+++ 6 Data Mining and Machine Learning/Assignments/Module_11/Module 11 Question 5.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 15 04:08:30 2025
+
+@author: ntcrw
+"""
+
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
+
+#------------------------------------------------------------------------------
+# Pre-question work -----------------------------------------------------------
+
+transactions = [['A', 'B', 'C'], ['A', 'C'], ['A', 'D'], ['B','E','F']]
+
+
+# Convert to string
+transactions = [[str(item) for item in transaction] for transaction in transactions]
+
+# Create the transaction encoder ----------------------------------------------
+tran_enc = TransactionEncoder()
+
+# Create transaction array with each elment -----------------------------------
+te_array = tran_enc.fit(transactions).transform(transactions)
+    
+# Create a dataframe to show the results of the encoding / transformation -----
+
+    
+encoded_transactions_df = pd.DataFrame(te_array, columns=tran_enc.columns_)
+
+#print(encoded_transactions_df)
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#
+# Question 5:
+#
+#With min_support = 0.25, what is the confidence of rule A->(B,C)? 
+# 
+
+# Find frequent itemsets with minimum support of 0.2 -------------------------
+frequent_itemsets = apriori(encoded_transactions_df, min_support=0.25, 
+                            use_colnames=True)
+    
+
+#print("Frequent item sets: ", frequent_itemsets)
+
+
+# Generate association rules with minimum confidence of 0.5 -------------------
+   
+assoc_rules = association_rules(frequent_itemsets, metric="confidence", 
+                          min_threshold=0)
+
+#print("\n Question1: \n", rules)
