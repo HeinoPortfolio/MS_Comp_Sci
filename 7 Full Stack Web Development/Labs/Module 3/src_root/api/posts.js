20f54b66b79e677a2e4e44c41ce8099b023a5579
Matthew Heino <61652462+HeinoPortfolio@users.noreply.github.com>
2025-09-07 14:48:54 -0400

    Add files via upload

---
+++ 7 Full Stack Web Development/Labs/Module 3/src_root/api/posts.js
@@ -0,0 +1,8 @@
+// Function for getting posts using a fetch method.
+export const getPosts = async (queryParams) =>{
+	const res = await fetch(
+		`${import.meta.env.VITE_BACKEND_URL}/posts?` + 
+			new URLSearchParams(queryParams),
+	)
+	return await res.json()
+}
